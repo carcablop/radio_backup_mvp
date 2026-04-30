485f7453101bce1bbefd8a0a97472f657d94beef
carcablop <carolina.cl19@gmail.com>
2026-04-30 16:30:40 -0500

    feat: se agrega implmentacion de ejmplo para normalizar nombre

--- src/radio_backup_mvp/normalizar_nombre.py
+++ src/radio_backup_mvp/normalizar_nombre.py
@@ -1,3 +1,9 @@
 '''Definir la funcion normalizar_nombre que reciba un string con un nombre de archivo y devuelva el mismo archivo normalizado.'''
 
-
+def normalizar_nombre(programa_archivo: list):
+    for prog_arch in programa_archivo:
+        n_archivo_normalizado = prog_arch["archivo"].strip().lower().replace(" ", "_").replace(",","_")
+        n_prog_normalizado = prog_arch['programa'].strip().lower().replace(" ","_").replace(",","_")
+        #imprimir el archivo normalizado (n_archivo_normalizado)
+        print(f"Archivo normalizado: {n_prog_normalizado}_{n_archivo_normalizado}")
+    return n_prog_normalizado + "_" + n_archivo_normalizado
